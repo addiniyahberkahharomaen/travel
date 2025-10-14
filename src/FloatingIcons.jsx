bbcbea63ac2b88aa7ed0b1853d41ae9adcd5e61e
addiniyahberkahharomaen <addiniyahberkahharomaen@gmail.com>
2025-10-14 15:29:05 +0700

    add links

--- src/FloatingIcons.jsx
+++ src/FloatingIcons.jsx
@@ -17,7 +17,7 @@ function FloatingIcons() {
       {/* Sosial Media Icons - kiri bawah */}
       <div className="fixed bottom-6 left-6 flex flex-col space-y-4 z-50">
         <a
-          href="https://instagram.com/"
+          href="https://www.instagram.com/addhintour_travel/"
           target="_blank"
           rel="noopener noreferrer"
           className="bg-gradient-to-tr from-yellow-500 via-pink-500 to-purple-600 text-white p-3 rounded-full shadow-md hover:opacity-90 transition transform hover:scale-110"
@@ -25,7 +25,7 @@ function FloatingIcons() {
           <FaInstagram size={22} />
         </a>
         <a
-          href="https://tiktok.com/"
+          href="https://www.tiktok.com/@addiniyahberkahharomaen"
           target="_blank"
           rel="noopener noreferrer"
           className="bg-black text-white p-3 rounded-full shadow-md hover:opacity-90 transition transform hover:scale-110"
@@ -33,7 +33,7 @@ function FloatingIcons() {
           <FaTiktok size={22} />
         </a>
         <a
-          href="https://youtube.com/"
+          href="www.youtube.com/@addiniyahberkahharomaen"
           target="_blank"
           rel="noopener noreferrer"
           className="bg-red-600 text-white p-3 rounded-full shadow-md hover:opacity-90 transition transform hover:scale-110"
